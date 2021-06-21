6fe1581aa1181ff68d725dbcdc71bada2076471e
leon318 <leonkornfeld@yahoo.com>
2021-06-21 04:00:33 -0700

    Initial Commit

---
+++ Arduino/weight_program/weight_program.ino
@@ -0,0 +1,23 @@
+#include "HX711.h"
+
+#define calibration_factor -20550 //This value is obtained using the SparkFun_HX711_Calibration sketch
+
+#define LOADCELL_DOUT_PIN  4
+#define LOADCELL_SCK_PIN  5
+
+HX711 scale;
+
+void setup() {
+  Serial.begin(9600);
+ 
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
+
+  
+}
+void loop() {
+  
+  Serial.print((scale.get_units() * -1), 1); //scale.get_units() returns a float
+  Serial.println();
+}
