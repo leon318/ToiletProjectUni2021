6e028d344752e30f307b635386b9d1034e204824
leon318 <leonkornfeld@yahoo.com>
2021-06-22 05:52:15 -0700

    Import file can graph one file at a time and add in a 0 value to the documents without 0.

--- Python/Import_generated_file.py
+++ Python/Import_generated_file.py
@@ -1,16 +1,15 @@
 from matplotlib import pyplot as plt
 import numpy as np
-import os
-
-with open("Weightfile") as f:
+import glob
+with open("/Users/Leon/Documents/ToiletProjectUni2021/Experiment1(Leon_Standing)/Experiment1,T2") as f:
     lines = f.readlines()  # read
 lines[0] = ("0.0" + "\n")  # you can replace zero with any line number
-with open("Weightfile", "w") as f:
+with open("/Users/Leon/Documents/ToiletProjectUni2021/Experiment1(Leon_Standing)/Experiment1,T2", "w") as f:
     f.writelines(lines) #write back
-data = np.loadtxt(fname='Weightfile', delimiter='\n')
+data = np.loadtxt(fname='/Users/Leon/Documents/ToiletProjectUni2021/Experiment1(Leon_Standing)/Experiment1,T2', delimiter='\n')
 print(data)
 y = data
-plt.plot(y)
+plt.plot(y, 'r')
 plt.title("Weight vs Measurements")
 plt.xlabel("Measurements")
 plt.ylabel("Weight")
