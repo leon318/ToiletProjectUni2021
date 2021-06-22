b8a1c7dc45fd00730658a56a8a57dc1d750b3860
leon318 <leonkornfeld@yahoo.com>
2021-06-22 02:00:10 -0700

    Setup to upload files to separate folder and do experiment

--- Python/toiletagain.py
+++ Python/toiletagain.py
@@ -3,13 +3,17 @@
 #read the serial data and print it as line
 import serial
 import time
+import os
+import shutil
+from pathlib import Path
+
 ser = serial.Serial('/dev/tty.usbmodem14201', baudrate=9600, timeout=.1)
 
-weight = []
+weight = [5]
 counter = 0
-f = open("Weightfile", "w")
+f = open("Experiment1,T10", "w")
 time.sleep(3)
-while counter < 100:
+while counter < 200:
     arduinoData = ser.readline()
     string = arduinoData.decode()  # convert the byte string to a unicode string
     num = str(string)  # convert the unicode string to an int
@@ -18,6 +22,9 @@ while counter < 100:
     f.write(str(num))
     f.write(str("\n"))
     counter += 1
+From1 = "/Users/Leon/Documents/ToiletProjectUni2021/Python/Experiment1,T10"
+To1 = "/Users/Leon/Documents/ToiletProjectUni2021/Experiment1(Leon_Standing)/Experiment1,T10"
+os.rename(From1,To1)
 
 
 
