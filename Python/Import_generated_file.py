64fd03a366e3dd5bcce4da6daac901b79d4b843c
leon318 <leonkornfeld@yahoo.com>
2021-06-22 00:59:44 -0700

    Import file with adding 0.0 as first measurement

--- Python/Import_generated_file.py
+++ Python/Import_generated_file.py
@@ -1,8 +1,14 @@
 from matplotlib import pyplot as plt
 import numpy as np
-data = np.loadtxt(fname= 'Weightfile', delimiter= '\n')
-print(data)
+import os
 
+with open("Weightfile") as f:
+    lines = f.readlines()  # read
+lines[0] = ("0.0" + "\n")  # you can replace zero with any line number
+with open("Weightfile", "w") as f:
+    f.writelines(lines) #write back
+data = np.loadtxt(fname='Weightfile', delimiter='\n')
+print(data)
 y = data
 plt.plot(y)
 plt.title("Weight vs Measurements")
