4c0e3ab24990537bcdc586a079fdf31436aa00fe
leon318 <leonkornfeld@yahoo.com>
2021-06-22 02:37:44 -0700

    Working multiple file plotter on same graph

---
+++ Python/Test_for_importing_multiple_files.py
@@ -0,0 +1,18 @@
+import glob
+from matplotlib import pyplot as plt
+import numpy as np
+
+print(glob.glob('/Users/Leon/Documents/ToiletProjectUni2021/Experiment1(Leon_Standing)/Experiment1,T*'))
+
+filenames = sorted(glob.glob('/Users/Leon/Documents/ToiletProjectUni2021/Experiment1(Leon_Standing)/Experiment1,T*'))
+filenames = filenames[0:10]
+for filename in filenames:
+    print(filename)
+    data = np.loadtxt(fname=filename, delimiter='\n')
+    print(data)
+    y = data
+    plt.plot(y)
+plt.title("Weight vs Measurements")
+plt.xlabel("Measurements")
+plt.ylabel("Weight")
+plt.show()
