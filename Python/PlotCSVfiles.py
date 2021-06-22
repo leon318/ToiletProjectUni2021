fc6b0047fe334c2029238a0f9a71c07260f434e1
leon318 <leonkornfeld@yahoo.com>
2021-06-22 07:31:32 -0700

    this can plot csv files with the time stamps on the x axis

---
+++ Python/PlotCSVfiles.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+import glob
+
+import numpy as np
+import pandas as pd
+print(glob.glob('/Users/Leon/Documents/ToiletProjectUni2021/Experiment3(Leon_Standing2)/Experiment3,T-*.csv'))
+filenames = sorted(glob.glob('/Users/Leon/Documents/ToiletProjectUni2021/Experiment3(Leon_Standing2)/Experiment3,T-*.csv'))
+filenames = filenames[0:5]
+for filename in filenames:
+  print(filename)
+  x,y = np.loadtxt(filename,
+                     unpack = True,
+                     delimiter= ',')
+  print(x)
+  print(y)
+  plt.plot(x,y)
+plt.title("Weight vs Time")
+plt.xlabel("Time")
+plt.ylabel("Weight")
+y.mean(axis=-1)
+plt.show()
+#t = pd.read_csv('/Users/Leon/Documents/ToiletProjectUni2021/Experiment3(Leon_Standing2)/Experiment3,T-1.csv')
+#t.head()
+
