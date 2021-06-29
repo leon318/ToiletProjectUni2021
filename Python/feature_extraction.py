2a77e54ff15533b5a03174d0eebeb45e0e63d273
leon318 <leonkornfeld@yahoo.com>
2021-06-29 02:48:06 -0700

    f string version

--- Python/feature_extraction.py
+++ Python/feature_extraction.py
@@ -2,31 +2,23 @@ import glob
 import numpy as np
 
 title = "Summary.csv"
-f = open(title, "w")
-f.write(",".join(['Person', 'Test', 'Position','Total_Weight', 'Floor_Weight', 'Toilet_Weight']))
-h = 0
-
 #TODO Put names into alphabetical order
 names = ('anjany', 'aron', 'kyriakos', 'leon', 'matteo')
+columns = ['Person', 'Test', 'Position','Total_Weight', 'Floor_Weight', 'Toilet_Weight']
 
+f = open(title, "w")
+f.write(f"{','.join(columns)}\n")
 files = (sorted(glob.glob("/Users/Leon/Documents/ToiletProjectUni2021/feature_extraction//*/*.csv")))
-print(files[0:10])
-f.write('\n')
-if 1:
-    for i, file in enumerate(files):
-        print(i)
-        #print('\n')
-        x, y, z = np.loadtxt(file,
-                             unpack=True,
-                             delimiter=',',
-                             skiprows=1)
-        if names[h] not in file:
-            h += 1
+h = 0
 
-        f.write(",".join([names[h], str(i)]))
-        if 'hunch' in file:
-            f.write(",hunched,")
-        elif 'straight' in file:
-            f.write(",straight,")
-        f.write((",".join([str(((y[-1] + z[-1]))), str(y[-1]), str(z[-1])])))
-        f.write('\n')
+for i, file in enumerate(files):
+    x, y, z = np.loadtxt(file,
+                         unpack=True,
+                         delimiter=',',
+                         skiprows=1)
+    if names[h] not in file:
+        h += 1
+
+    name = names[h]
+    position = 'hunched' if 'hunch' in file else 'straight'
+    f.write(f"{name},{i},{position},{y[-1] + z[-1]:.3f},{y[-1]:.3f},{z[-1]:.3f}\n")
