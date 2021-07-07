ecc07dbbb25be206471da8a305031a2adbb0cbef
leon318 <leonkornfeld@yahoo.com>
2021-07-07 04:31:00 -0700

    generates arrays of both position and person in 0 and 1

--- Python/model_preparation.py
+++ Python/model_preparation.py
@@ -1,8 +1,13 @@
+import numpy as np
 from Python.data_analysis_functions.feature_position_tuple_list_generator import tuple_list_generator
 from Python.data_analysis_functions.array2d_generator import array_2d_generator
 from Python.data_analysis_functions.array_randomizer import array_randomizer_and_tupple_generator
-final_list = tuple_list_generator()
+final_list, input_integer, encoded, position_and_person = tuple_list_generator()
+# print(final_list)
 feature_matrix, position_matrix = array_2d_generator(final_list)
+
+# character_matrix = np.concatenate((encoded,position_matrix),axis=1)
+# print(character_matrix)
 
 num_rows1, num_cols1 = feature_matrix.shape
 num_rows2, num_cols2 = position_matrix.shape
@@ -11,13 +16,13 @@ num_rows2, num_cols2 = position_matrix.shape
 # print("position matrix", num_rows2)
 # print("postion matrix", num_cols2)
 
-training_tupple, testing_tuple = array_randomizer_and_tupple_generator(feature_matrix, position_matrix)
-print(testing_tuple)
-
-
-
-
-# print(test)
+training_tuple, testing_tuple = array_randomizer_and_tupple_generator(feature_matrix, position_and_person, input_integer)
+# print(testing_tuple)
+#
+#
+#
+#
+# # print(test)
 
 
 
