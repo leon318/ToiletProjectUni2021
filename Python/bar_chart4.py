b48f166e82cda5e60c7855b16319a1c406eadc50
leon318 <leonkornfeld@yahoo.com>
2021-07-09 04:42:20 -0700

    caps on error bars

--- Python/bar_chart4.py
+++ Python/bar_chart4.py
@@ -23,11 +23,12 @@ std_list_hunched_toilet= list()
 std_list_straight_toilet = list()
 for i, name in enumerate(names):
     for j, position in enumerate(positions):
-        a = df[(df['Person'] == names[i])]
+        a = df[(df['Person'] == names[i]) & (df['Position'] == positions[j])]
         tot = a['Total_Weight'].mean()
         norm_floor_data = a['Floor_Weight']/tot
         norm_toilet_data = a['Toilet_Weight']/tot
-        # print(f"{names[i]}, {positions[j]} :")
+        print(f"{names[i]}, {positions[j]} :")
+        print(norm_toilet_data)
         mean_floor_data = norm_floor_data.mean()
         std_floor = norm_floor_data.std()
         mean_toilet_data = norm_toilet_data.mean()
@@ -37,23 +38,26 @@ for i, name in enumerate(names):
             std_list_hunched_floor.append(std_floor)
             mean_list_hunched_toilet.append(mean_toilet_data)
             std_list_hunched_toilet.append(std_toilet)
-        else:
+        elif positions[j] == 'straight':
             mean_list_straight_floor.append(mean_floor_data)
             std_list_straight_floor.append(std_floor)
             mean_list_straight_toilet.append(mean_toilet_data)
             std_list_straight_toilet.append(std_toilet)
-
-
+# print(mean_list_hunched_toilet)
+# print(mean_list_straight_toilet)
+# print(mean_list_hunched_floor)
+# print(mean_list_straight_floor)
 x = np.arange(len(names))
 
-width = 0.25
+width = 0.2
 fig, ax = plt.subplots()
-rects1 = ax.bar(x-width/2, mean_list_hunched_floor, width, label='Hunched Floor', yerr=std_list_hunched_floor)
-rects2 = ax.bar(x+width/2, mean_list_hunched_toilet, width, label='Hunched Toilet', yerr=std_list_hunched_toilet)
-rects3 = ax.bar(x - width / 2, mean_list_straight_floor, width, label='Straight Floor', yerr=std_list_straight_floor)
-rects4 = ax.bar(x + width / 2, mean_list_straight_toilet, width, label='Straight Toilet', yerr=std_list_straight_toilet)
-ax.set_xticks(x)
+rects1 = ax.bar(x, mean_list_hunched_floor, width, label='Hunched Floor', yerr=std_list_hunched_floor, edgecolor='black', ecolor='black', capsize=5 )
+rects2 = ax.bar(x+width, mean_list_hunched_toilet, width, label='Hunched Toilet', yerr=std_list_hunched_toilet, edgecolor='black', ecolor='black', capsize=5)
+rects3 = ax.bar(x + (2*width), mean_list_straight_floor, width, label='Straight Floor', yerr=std_list_straight_floor, edgecolor='black', ecolor='black', capsize=5)
+rects4 = ax.bar(x + (3*width), mean_list_straight_toilet, width, label='Straight Toilet', yerr=std_list_straight_toilet, edgecolor='black', ecolor='black', capsize=5)
+ax.set_xticks(x + width + width/2)
 ax.set_xticklabels(names)
+ax.set_xlabel('People')
 ax.set_ylabel('Normalized Mean Weight')
 ax.set_title('Normalized Mean Weight vs People in Hunched or Straight Positions')
 # ax.bar_label(rects1, padding=3)
